fadb3a3ffe259bd4281b0b8b3c9f6c47c1bbafe5
Carlos <carlosfv2002@gmail.com>
2021-11-11 12:53:55 +0100

    Commit 1. Creada clase app, atributos y constructor

---
+++ App.java
@@ -0,0 +1,15 @@
+public class App{
+
+    // Nombre de la app
+    private String nombre;
+    // Tipo de la app
+    private String tipo;
+
+    /**
+     * Crea estructura de la app
+     */
+    public App(String nombreApp, String tipoSerie) {
+        nombre = nombreApp;
+        tipo = tipoSerie;
+    }
+}

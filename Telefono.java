c9463ac4b5b91e21aa00b43d915efc55e6638344
Carlos <carlosfv2002@gmail.com>
2021-11-11 13:27:00 +0100

    Commit 5. Creados getters y setters en clase telefono

--- Telefono.java
+++ Telefono.java
@@ -15,3 +15,46 @@ public class Telefono{
         bateria = porcentajeBateria;
         app = nombreApp;
     }
+    
+    /**
+     * Devuelve la marca del movil
+     */
+    public String getMarca() {
+        return marca;
+    }   
+    
+    /**
+     * Devuelve la batería del movil
+     */
+    public int getBateria() {
+        return bateria;
+    }  
+    
+    /**
+     * Devuelve el nombre de la app
+     */
+    public App getNombreApp() {
+        return app;
+    }  
+    
+    /**
+     * Establece nombre de la marca
+     */
+    public void setMarca(String nombreNuevoMarca) {
+        marca = nombreNuevoMarca;
+    }   
+    
+    /**
+     * Establece nombre del modelo
+    */
+    public void setBateria(int nuevaBateria) {
+        bateria = nuevaBateria;
+    }  
+    
+    /**
+     * Establece el nombre de la app
+    */
+    public void setApp(App nombreNuevoApp) {
+        app = nombreNuevoApp;
+    }
+}
